c50facd45f97f1a80de44b784b8aca0d9fa78086
Kristen Haydel <kristen@whatever.com>
2015-12-31 12:26:35 -0600

    All tests are passing

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,11 +2,15 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
-  console.log($scope);
-  console.log(Links);
   $scope.data = {};
   $scope.data.links;
   $scope.getLinks = function(){
     Links.getLinks()
-  }
+    .then(function(links){
+      //getting link, do what with it? 
+      $scope.data.links = links;
+      console.log('scope data links', $scope.data.links);
+    });
+  };
+  $scope.getLinks(); 
 });
